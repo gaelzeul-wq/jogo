98176bfbc98e863118d0f6552d537a4b5b3aac25
gael <gaelzeul@gmail.com>
2026-04-11 01:11:00 -0300

    1.2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let pontos = 0;
 let auto = 0;
 let auto2 = 0;
+let auto3 = 0;
+const LIMITE =1000000000;
+let venceu = false;
 
 function clicou(){
-    pontos++;
+    pontos+=1;
     atualizar();
 }
 function comprarAuto(){
@@ -14,9 +17,16 @@ function comprarAuto(){
     }
 }
 function comprarAuto2(){
-    if(pontos >= 100){
-        pontos -= 100;
+    if(pontos >= 1000){
+        pontos -= 1000;
         auto2+=10;
+        atualizar();
+    }
+}
+function comprarAuto3(){
+    if(pontos >= 10000){
+        pontos -= 10000;
+        auto3+=1000;
         atualizar();
     }
 }
@@ -24,8 +34,14 @@ function atualizar(){
     document.getElementById("pontos").innerText = pontos;
     document.getElementById("auto").innerText = auto;
     document.getElementById("auto2").innerText = auto2;
+    document.getElementById("auto3").innerText = auto3;
+
+    if(pontos >= LIMITE && !venceu){
+        venceu = true;
+        alert("Parabéns, você venceu o jogo!");
+    }
 }
 setInterval(function(){
-    pontos += auto + auto2;
+    pontos += auto + auto2 + auto3;
     atualizar();
-},600);
+},1000);
